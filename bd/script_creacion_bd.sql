2391c11d964c4acbb3fdd3050cee731b2a4a71e5
FABRICA <fabrica@fabrica.com>
2026-03-02 16:22:58 -0600

    Script Base de Datos ruiz_db Juan Carlos Ruiz Barcenas

--- bd/script_creacion_bd.sql
+++ bd/script_creacion_bd.sql
@@ -1,12 +1,16 @@
-CREATE DATABASE IF NOT EXISTS bd_garcia;
+CREATE DATABASE bd_ruiz;
 
-USE bd_garcia;
+USE bd_ruiz;
 
-CREATE TABLE IF NOT EXISTS person (
+CREATE TABLE person (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(255),
-    apellido VARCHAR(255),
+    nombre VARCHAR(100),
+    apellido VARCHAR(100),
     fechaNacimiento DATE,
-    puesto VARCHAR(255),
-    sueldo DECIMAL(10, 2)
+    puesto VARCHAR(100),
+    sueldo DECIMAL(10,2)
 );
+
+CREATE USER 'conexion'@'localhost' IDENTIFIED BY '123456';
+GRANT ALL PRIVILEGES ON bd_ruiz.* TO 'conexion'@'localhost';
+FLUSH PRIVILEGES;
